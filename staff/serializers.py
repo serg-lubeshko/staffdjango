81e4d8591f2e4e089784f8171937f600be0d3e57
Sergei_L <posse7297@gmail.com>
2021-06-10 16:33:55 +0300

    create Serializer

--- staff/serializers.py
+++ staff/serializers.py
@@ -5,4 +5,4 @@ from .models import Staff
 class StaffSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Staff
-        fields = ['surname', 'wage']
+        fields = ['surname', 'wage','staffs']
