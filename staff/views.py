12a7dd892783541b28cd05e9096cea5cb4084db3
Sergei_L <lubeshkosergei@gmail.com>
2021-06-10 22:01:45 +0300

    create filter

--- staff/views.py
+++ staff/views.py
@@ -6,9 +6,10 @@ from .models import Staff
 
 
 class StaffViewSet(viewsets.ModelViewSet):
+
     permission_classes = [permissions.IsAuthenticated]
     queryset = Staff.objects.all().order_by('surname')
     serializer_class = StaffSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_fields = ('level',)
-    http_method_names = ["get"]
+    http_method_names = ["get",]
